7ebad7de2a8e6b8ab8e0db4925fb9289f60ee598
rugved0102 <rugvedjunghare@gmail.com>
2024-08-16 22:14:52 +0530

    factorial solved using golden ratio

--- Recursion/fibonacciNo.java
+++ Recursion/fibonacciNo.java
@@ -1,7 +1,12 @@
 class fibonacciNo {
     public static void main(String[] args) {
-        System.out.println(fibo(7));;
+       System.out.println(fibo(50));;
+       //System.out.println(fiboFormula(50));
     }
+    static int fiboFormula(int n) {
+        return (int) (Math.pow(((1 + Math.sqrt(5))/2),n)/Math.sqrt(5));
+    }
+
     public static int fibo(int n) {
         if(n<2) {
             return n;

a9679a31bb841c84b94f8cc2440e2ee12c6624ff
rugved0102 <rugvedjunghare@gmail.com>
2024-08-17 19:50:35 +0530

    leetcode question 7

---
+++ leetcode questions/Reverse_Integer.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/reverse-integer/
+
+class Reverse_Integer {
+    public int reverse(int x) {
+        long res = 0;
+        while(x != 0) {
+            res = res*10 + x%10;
+            x = x/10;
+        }
+        if(res < Integer.MIN_VALUE || res > Integer.MAX_VALUE) 
+            return 0;
+        else
+            return (int) res;
+    }
+}

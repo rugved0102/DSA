36cac338c5cc65a1350428eccae3cbe5b2429c41
rugved0102 <rugvedjunghare@gmail.com>
2024-12-05 22:31:18 +0530

    none

---
+++ GFG160/Array/Second_Largest.java
@@ -0,0 +1,25 @@
+public class Second_Largest {
+    
+        public static int upperBound(int []arr, int x, int n){
+            int s = 0;
+            int e = arr.length-1;
+    
+            while(s <= e) {
+                int m = s + (e-s)/2;
+    
+                if(arr[m] > x){
+                    e = m - 1;
+                }
+                else if (arr[m] < x) {
+                    s = m + 1;
+                }
+                else {
+                    return m;
+                }
+                
+            }
+            return e;
+        }
+    
+    
+}

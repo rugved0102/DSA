a50955f4a36a891e0858c92b7129bc35ee392742
rugved0102 <rugvedjunghare@gmail.com>
2024-12-08 21:37:29 +0530

    recursion

--- Recursion/reverse_no.java
+++ Recursion/reverse_no.java
@@ -1,11 +1,30 @@
 public class reverse_no {
+   static int sum = 0;
+   static int reversed = 0;
+    public static void rev(int n) {
+        
+        if(n == 0) {
+            return;
+        }
+        int rem = n % 10;
+        sum = sum*10 + rem;
+        rev(n/10);
+        //return sum;
+        
+    }
+    public static int rev1(int n,int reversed) {
+        
+        if(n == 0) {
+            return reversed;
+        }
+        
+        return rev1(n/10, reversed*10 + n%10);
+    }
     public static void main(String[] args) {
         rev(1234);
-    }
-    private static int rev(int n) {
-        if(n%10 == n) {
-            return n;
-        }
-        return rev(n/10) + n%10;
+        //System.err.println(rev(1234));
+        System.err.println(rev1(1234,0));
+        System.out.println(sum);
+        
     }
 }

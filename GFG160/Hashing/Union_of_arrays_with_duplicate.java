b6e8635e2d7e4cc72df518de37a15ee3bd32fb61
rugved0102 <rugvedjunghare@gmail.com>
2024-12-30 21:31:35 +0530

    GFG160 Union of arrays with duplicate question added

---
+++ GFG160/Hashing/Union_of_arrays_with_duplicate.java
@@ -0,0 +1,14 @@
+class Union_of_arrays_with_duplicate {
+    public static int findUnion(int a[], int b[]) {
+        // code here
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int i = 0; i < a.length; i++) {
+            set.add(a[i]);
+        }
+        for(int i = 0; i < b.length; i++) {
+            set.add(b[i]);
+        }
+        return set.size();
+    }
+}

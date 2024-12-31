bc5d150f3757e15666058b9cb525d513433af05f
rugved0102 <rugvedjunghare@gmail.com>
2024-12-31 15:28:36 +0530

    GFG160 Longest Consecutive Subsequence

---
+++ GFG160/Hashing/Longest_Consecutive_Subsequence.java
@@ -0,0 +1,21 @@
+class Longest_Consecutive_Subsequence {
+    public int longestConsecutive(int[] arr) {
+        // code here
+        Arrays.sort(arr);
+        
+        int res = 1, cnt = 1;
+        
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i]==arr[i-1])
+            continue;
+            
+            if(arr[i] == arr[i-1]+1) {
+                cnt++;
+            } else {
+                cnt = 1;
+            }
+            res = Math.max(res, cnt);
+        }
+        return res;
+    }
+}

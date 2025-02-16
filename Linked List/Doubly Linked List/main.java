69baeafb96fd4e3258b65d738142489789ba135f
rugved0102 <rugvedjunghare@gmail.com>
2025-02-16 23:28:29 +0530

    Linked List

--- Linked List/Doubly Linked List/main.java
+++ Linked List/Doubly Linked List/main.java
@@ -5,6 +5,7 @@ public class main {
 
         list.insertFirst(10);
         list.insertFirst(20);
+        list.insertFirst(90);
         list.insertFirst(30);
         list.insertLast(40);
         list.insert(490, 1);

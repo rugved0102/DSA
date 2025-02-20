c0c03218db8e9cc896c6796836dd0ed252d3c901
rugved0102 <rugvedjunghare@gmail.com>
2025-02-20 21:44:38 +0530

    Strings Leetcode

---
+++ leetcode questions/Strings/Find_Unique_Binary_String.java
@@ -0,0 +1,14 @@
+public class Find_Unique_Binary_String {
+    public String findDifferentBinaryString(String[] nums) {
+        StringBuilder res = new StringBuilder();
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i].charAt(i) == '0') {
+                res.append('1');
+            }
+            else {
+                res.append('0');
+            }
+        } 
+        return res.toString();
+    }
+}

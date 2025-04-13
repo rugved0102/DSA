f3e2a28595a17655a14d387b6e2a8b7c9501478c
rugved0102 <rugvedjunghare@gmail.com>
2025-04-13 13:51:25 +0530

    queue

---
+++ Queues/queue1.java
@@ -0,0 +1,17 @@
+// package Queues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class queue1 {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+    }
+}

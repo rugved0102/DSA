2f844f0f476f15a03660774ba45193edbac1c7e1
rugved0102 <rugvedjunghare@gmail.com>
2025-04-13 15:17:37 +0530

    Dynamic Stack added

--- Stacks/StackMain.java
+++ Stacks/StackMain.java
@@ -2,13 +2,14 @@
 
 public class StackMain {
     public static void main(String[] args) throws StackException {
-        CustomStack stack = new CustomStack(5);
+        // CustomStack stack = new CustomStack(5);
+        DynamicStack stack = new DynamicStack(5);
         stack.push(0);
         stack.push(1);
         stack.push(2);
         stack.push(3);
         stack.push(4);
-        // stack.push(5);
+        stack.push(5);
 
         System.out.println(stack.pop());
         System.out.println(stack.pop());

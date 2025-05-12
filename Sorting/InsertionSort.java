d1ce7306c6f36bd0136a55cbbaee4f0b08108500
rugved0102 <rugvedjunghare@gmail.com>
2025-05-12 16:45:25 +0530

    insertion sort

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -2,7 +2,8 @@ import java.util.Arrays;
 
 public class InsertionSort {
     public static void main(String[] args) {
-         int[] arr = {3,5,4,1,2};
+        //  int[] arr = {3,5,4,1,2};
+         int[] arr = {5,4,3,2,1};
         System.out.println(Arrays.toString(insertion(arr)));;
     }
     static int[] insertion(int[] arr) {
@@ -10,9 +11,10 @@ public class InsertionSort {
             for(int j = i+1; j > 0; j--) {
                 if(arr[j-1]>arr[j]) {
                     swap(arr, j-1, j);
-                }else {
-                    break;
                 }
+                // else {
+                //     break;
+                // }
             }
         }
         return arr;
